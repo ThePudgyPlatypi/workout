d9aea7de394c4ad49850bf01d33614acc1ebd820
ThePudgyPlatypi <cstehm@comcast.net>
2017-09-22 17:11:28 -0400

    working db structure

--- workout/app/assets/javascripts/app.js
+++ workout/app/assets/javascripts/app.js
@@ -1,6 +1,8 @@
 var app = angular.module('workout', ['ui.router', 'templates', 'Devise']);
 
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-	
+	$stateProvider.state('home', {
+		
+	})
 }]);
 
